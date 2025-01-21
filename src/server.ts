d14a4f4e9bedef23283fe7bb0090eb073a1db8ac
Luis Gustavo <75265319+luisGust4vo@users.noreply.github.com>
2025-01-21 19:26:47 -0300

    Feat:configuracao do banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,10 @@
 import dotenv from "dotenv";
 import app from "./app";
 
-// Carregar as variáveis de ambiente
 dotenv.config({ path: ".env.development" });
 
-// Garantir que o PORT seja um número
 const PORT: number = parseInt(process.env.PORT as string, 10);
 
-// Verificar se o PORT é válido (se a conversão falhou)
 if (isNaN(PORT)) {
   throw new Error("PORT deve ser um número válido.");
 }
