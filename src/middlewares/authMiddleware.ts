979b0e8d354ec84d5b1ccfef05d618486bba50ee
Luis Gustavo <75265319+luisGust4vo@users.noreply.github.com>
2025-01-22 00:45:47 -0300

    Feat: Mudanca pro middleware

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -11,7 +11,7 @@ export const authMiddleware = (
   res: Response,
   next: NextFunction
 ): void => {
-  const token = req.headers.authorization?.split(" ")[1]; // Formato: "Bearer TOKEN"
+  const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) {
     res.status(401).json({ error: "Token não fornecido." });
