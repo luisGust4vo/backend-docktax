5af7d7ea168288a48fb52e5ec9b9c8c1dac4f619
Luis Gustavo <75265319+luisGust4vo@users.noreply.github.com>
2025-01-21 21:51:53 -0300

    Feat: Criacao e refatoracao do codigo

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -8,7 +8,7 @@ router.post(
   async (req: Request, res: Response): Promise<void> => {
     try {
       const newUser = await userController.createUser(req, res);
-      res.status(201).json({ message: "Usuário criado com sucesso." }); //
+      res.status(201).json({ message: "Usuário criado com sucesso." });
     } catch (error) {
       res.status(500).json({ error: "Erro ao criar usuário." });
     }
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,16 +1,12 @@
-import { PrismaClient } from "@prisma/client"; // Correção na importação
+import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
 import { CreateUserDTO } from "../dtos/create-user.dto";
 
-// Criar uma instância do PrismaClient
 const prisma = new PrismaClient();
-
 export class UserService {
-  // Método para criar um novo usuário
   async createUser(data: CreateUserDTO) {
     const { email, name, password } = data;
 
-    // Verificar se o email já existe
     const existingUser = await prisma.user.findUnique({
       where: { email },
     });
@@ -19,10 +15,8 @@ export class UserService {
       throw new Error("Usuário já cadastrado.");
     }
 
-    // Hash da senha
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Criar usuário no banco
     const newUser = await prisma.user.create({
       data: {
         email,
