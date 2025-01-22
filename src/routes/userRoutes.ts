874b68fc8fee9987c521d508ec8c8074033e0aff
Luis Gustavo <75265319+luisGust4vo@users.noreply.github.com>
2025-01-21 21:42:56 -0300

    Feat:Criacao da API do  usuario

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -3,13 +3,16 @@ import { UserController } from "../controllers/user.controller";
 
 const router = express.Router();
 const userController = new UserController();
-router.post("/users", async (req: Request, res: Response): Promise<void> => {
-  try {
-    const newUser = await userController.createUser(req, res);
-    res.status(201).json(newUser);
-  } catch (error) {
-    res.status(500).json({ error: "Erro ao criar usuário." });
+router.post(
+  "/createUser",
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const newUser = await userController.createUser(req, res);
+      res.status(201).json({ message: "Usuário criado com sucesso." }); //
+    } catch (error) {
+      res.status(500).json({ error: "Erro ao criar usuário." });
+    }
   }
-});
+);
 
 export default router;
